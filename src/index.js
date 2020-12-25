90f0d58f30c443f6488fa17ef9b8b867cfa5fe38
sputja <sputja@gmail.com>
2020-12-25 13:04:44 +0200

    start

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import './styles.css';
+// import './js/theme.js';
+// import menuData from './js/menu.json';
+// import template from './template/menu.hbs';
+
+// const menuRef = document.querySelector('.js-menu');
+// const markup = template(menuData);
+// menuRef.insertAdjacentHTML('afterbegin', markup);
